8c72886e7927bc9007eb23ea9c51f677bd908fc5
szatsepa <arcady.1254@gmail.com>
2012-02-21 17:53:45 +0200

    sound player#

--- main/view.php
+++ main/view.php
@@ -78,14 +78,14 @@ if($presentation[$key][sound]){
 ?>
 <br/>
 <div id="players">
-    
-        <object type="application/x-shockwave-flash" data="./flash/audio_player.swf" id="audioplayer1" height="24" width="288">  
+ <object type="application/x-shockwave-flash" data="./flash/dewplayer-vol.swf?mp3=<?php echo $this_sound;?>&autostart=yes" width="240" height="20" id="dewplayer-vol"><param name="wmode" value="transparent" /><param name="movie" value="./flash/dewplayer-vol.swf?mp3=<?php echo $this_sound;?>&autostart=yes" /></object>   
+<!--        <object type="application/x-shockwave-flash" data="./flash/audio_player.swf" id="audioplayer1" height="24" width="288">  
         <param name="movie" value="./flash/audio_player.swf"></param>  
         <param name="FlashVars" value="playerID=1&autostart=yes&loop=no&bg=0xffffff&loader=0xcccccc&border=0xcccccc&text=0x8c8c8c&leftbg=0xcccccc&lefticon=0xffffff&righticon=0xffffff&slider=0xcccccc&rightbg=0xcccccc&soundFile=<?php echo $this_sound;?>"></param>  
         <param name="quality" value="high"></param>  
         <param name="menu" value="false"></param>  
         <param name="wmode" value="transparent"></param>  
-    </object>  
+    </object>  -->
 </div>
 <?php }?>
 <div>
